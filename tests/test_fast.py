d13b9fe123798d7b7dac9cb3ecfc4ce18e8af8c2
Anne Atse <vicky69200@gmail.com>
2024-08-29 19:51:45 +0200

    nouveau message

--- tests/test_fast.py
+++ tests/test_fast.py
@@ -4,15 +4,15 @@ from apiannegit import app  # Replace 'apiannegit' with the actual name of your 
 client = TestClient(app)
 
 def test_index():
-    response = client.get("/")
+    response = client.get('/')
     assert response.status_code == 200
-    assert response.json() == {"message": "Hello, stranger, from GitHub actions"}
+    assert response.json() == {'message': 'Hello, stranger, from GitHub actions'}
 
 def test_get_name():
-    response = client.get("/John")
+    response = client.get('/John')
     assert response.status_code == 200
-    assert response.json() == {"message": "Hello, John"}
+    assert response.json() == {'message': 'Hello, John'}
 
-    response = client.get("/Jane")
+    response = client.get('/Jane')
     assert response.status_code == 200
-    assert response.json() == {"message": "Hello, Jane"}
+    assert response.json() == {'message': 'Hello, Jane'}
