2fe4f7590d9d55b6089e92d0862d6aa2ef0ba53e
Anne Atse <vicky69200@gmail.com>
2024-08-29 14:40:42 +0200

    modification

--- apiannegit.py
+++ apiannegit.py
@@ -1,27 +1,22 @@
 # 1. Library imports
 import uvicorn
 from fastapi import FastAPI
-from irisapitest import IrisModel  # Importer votre modèle ML depuis irisapitest
 
 # 2. Create the app object
 app = FastAPI()
 
-# 3. Load the ML model
-model = IrisModel()  # Assurez-vous que cette classe existe dans irisapitest
-
-# 4. Index route, opens automatically on http://127.0.0.1:8000
+# 3. Index route, opens automatically on http://127.0.0.1:8000
 @app.get('/')
 def index():
-    return {'message': 'Welcome to the Iris API'}
+    return {'message': 'Hello, stranger'}
 
-# 5. Predict route, expects features as query parameters and returns the prediction
-@app.get('/predict/')
-def predict(sepal_length: float, sepal_width: float, petal_length: float, petal_width: float):
-    features = [[sepal_length, sepal_width, petal_length, petal_width]]
-    prediction = model.predict(features)
-    return {'prediction': prediction}
+# 4. Route with a single parameter, returns the parameter within a message
+#    Located at: http://127.0.0.1:8000/AnyNameHere
+@app.get('/{name}')
+def get_name(name: str):
+    return {'message': f'Hello, {name}'}
 
-# 6. Run the API with uvicorn
+# 5. Run the API with uvicorn
 #    Will run on http://127.0.0.1:8000
 if __name__ == '__main__':
     uvicorn.run(app, host='127.0.0.1', port=8000)
